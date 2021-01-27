a4b68834a3bc3de7f3f79e3abab7c101b5dbbf6e
rayat69 <rayathossain49@gmail.com>
2021-01-27 15:12:48 +0600

    gas

---
+++ front-end/src/App.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import io from "socket.io-client";
+
+interface Chat {
+  message: String;
+  name: String;
+}
+
+const socket: SocketIOClient.Socket = io.connect("http://localhost:4321");
+
+const App: React.FC = () => {
+  const [state, setState] = useState({ message: "", name: "" });
+  const [chat, setChat] = useState<Chat[]>([
+    { message: "sfsf", name: "sfsdf" },
+  ]);
+
+  useEffect(() => {
+    socket.on("message", (param: { name: String; message: String }) => {
+      const { name, message } = param;
+      // gas.push({ name, message });
+      // setChat(gas);
+      // console.log(gas);
+
+      setChat([...chat, { name, message }]);
+    });
+  }, [chat]);
+  
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(state);
+
+    e.preventDefault();
+    const { name, message } = state;
+    socket.emit("message", { name, message });
+    setState({ message: "", name });
+  };
+
+  const onTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  // const renderChat = () => {
+  //   return chat.map(({ name, message }, idx) => (
+  //     <div key={idx}>
+  //       <h3>
+  //         {name}: <span>{message}</span>
+  //       </h3>
+  //     </div>
+  //   ));
+  // };
+
+  return (
+    <div className="App">
+      <form onSubmit={onSubmitHandler}>
+        <h1>Messenger</h1>
+        <div className="name-field">
+          <input
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={onTextChange}
+          />
+        </div>
+        <div className="message-field">
+          <input
+            type="text"
+            name="message"
+            value={state.message}
+            onChange={onTextChange}
+          />
+        </div>
+        <button type="submit">Send message</button>
+      </form>
+      <div className="render-chat">
+        <h1>Render chat</h1>
+        {chat.map(({ name, message }, idx) => (
+          <div key={idx}>
+            <h3>
+              {name}: <span>{message}</span>
+            </h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
